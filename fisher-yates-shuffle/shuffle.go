6b45e9b356e36a505123ae3fe7dc42613dc2be97
John Pettigrew <john.pettigrew404@gmail.com>
2015-12-03 22:39:07 -0600

    Run gofmt

--- fisher-yates-shuffle/shuffle.go
+++ fisher-yates-shuffle/shuffle.go
@@ -1,24 +1,24 @@
 package main
 
-import(
-  "fmt"
-  "math/rand"
-  "time"
+import (
+	"fmt"
+	"math/rand"
+	"time"
 )
 
 func shuffle(list *[]string) {
-  currentList := *list
-  rand.Seed(time.Now().Unix())
+	currentList := *list
+	rand.Seed(time.Now().Unix())
 
-  for remaining := len(currentList); remaining > 0; remaining-- {
+	for remaining := len(currentList); remaining > 0; remaining-- {
 
-    random := rand.Intn(remaining)
-    currentList[random], currentList[remaining - 1] = currentList[remaining - 1], currentList[random]
-  }
+		random := rand.Intn(remaining)
+		currentList[random], currentList[remaining-1] = currentList[remaining-1], currentList[random]
+	}
 }
 
-func main(){
-  list := []string{"a", "b", "c", "d", "e", "f", "g"}
-  shuffle(&list)
-  fmt.Println(list)
+func main() {
+	list := []string{"a", "b", "c", "d", "e", "f", "g"}
+	shuffle(&list)
+	fmt.Println(list)
 }
